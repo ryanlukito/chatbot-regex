e7108cdad4fca61c1c8f450fa92a1ae07e74439f
ryanlukito <ryanlukito13@gmail.com>
2024-09-02 08:52:29 +0700

    msg: update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 # ChatBot RegEx
-def chatbot_regex():
-    return 1
+import re
+
+chatbotResponse = {} # Tinggal ditentuin chatbotnya mau respon apa (dibikin dictionary)
+
+defResponse = {} # default response ke user kalo inputnya gk sesuai template
+
+def getResponse(userInput):
+    for pattern, response in chatbotResponse.items():
+        if re.search(pattern, userInput, re.IGNORECASE):
+            return response
+    return defResponse
+
+def chatbotRegex():
+    print('Chatbot: ')
+    while True:
+        userInput = input('You: ')
+        if re.search(r'bye|goodbye', userInput, re.IGNORECASE):
+            print('Chatbot: Bye.')
+            break
+        response = getResponse(userInput)
+        print(f'Chatbot: {response}')
 
 if __name__ == "__main__":
-    chatbot_regex()
+    chatbotRegex()
