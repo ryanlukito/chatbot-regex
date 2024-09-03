ce1e0a54dd1f4ca01b76d9db78fee33588d0d916
ryanlukito <ryanlukito13@gmail.com>
2024-09-03 20:06:35 +0700

    msg: update rules

--- modules/RegexChatbot.py
+++ modules/RegexChatbot.py
@@ -9,13 +9,16 @@ class ChatBot:
         if chatbotResponse is None:
             chatbotResponse = {
                 r'hi|hello|hey|hai|hallo|halo': 'Halo, ada yang saya bisa bantu?',
-                r'siapa namamu?': 'Saya adalah ruled based chatbot yang dibuat oleh Alex & Ryan. Saya dibuat untuk menemani kamu',
-                r'apa tujuanmu dibuat?': 'Sekedar basa-basi saja',
-                r'apakah kamu suka (.*)': 'I do not have preferences, but I enjoy helping you!',
-                r'ya saya memiliki pertanyaan': 'Baiklah, apa pertanyaan Anda?',
-                r'bagaimana|apa kabarmu': 'Saya baik-baik saja, bagaimana dengan kamu?',
-                r'siapa .*mu': 'Saya adalah ruled based chatbot yang dibuat oleh Alex & Ryan.',
-                r'thank you|thanks|terimakasih|makasih .*': 'Sama-sama, jika Anda punya pertanyaan lebih lanjut silakan tanyakan saja!',
+                r'siapa namamu?': 'Saya adalah ruled based chatbot yang dibuat oleh Alex & Ryan, mahasiswa DTETI FT UGM',
+                r'apa tujuanmu dibuat?': 'Memberikan informasi mengenai DTETI, mulai dari perkuliahan sampai kehidupan mahasiswa/i-nya',
+                r'saya|aku memiliki pertanyaan': 'Baiklah, apa pertanyaan Anda?',
+                r'apakah kamu tahu tentang (.*)': 'Tentu apakah {0} menurutmu menarik? Jelaskan pada saya!',
+                r'menurut (saya|aku), (.*)':'Aku setuju dengan pendapatmu!',
+                r'(.*) berkuliah di DTETI':'Saya sangat penasaran bagaimana rasanya, jelaskan pada saya!',
+                r'(.*)?sulit|susah (.*)': 'Bisakah anda jelaskan dimana susahnya?',
+                r'menurut (saya|aku), (susah|sulit)nya itu (.*)': 'Tenang saja, yang kamu rasakan itu valid. Tetap semangat ya!',
+                r'(.*)?siapa yang paling (ganteng|baik|tidak rasis|cerdas|positif) di DTETI?': 'Wah jelas Alex Cinatra Hutasoit', #Easter Egg
+                r'thank you|thanks|terimakasih|makasih (.*)?': 'Sama-sama, jika Anda punya pertanyaan lebih lanjut silakan tanyakan saja!',
                 r'bye|goodbye|selamat tinggal|oke.*|baiklah|terimakasih': 'Goodbye! Have a great day!'
             }
 
