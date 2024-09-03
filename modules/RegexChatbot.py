5743f4a8ae9b224c8ec37287b6be11ce46fa4619
alexchtst-01 <acinatra@gmail.com>
2024-09-03 20:18:17 +0700

    adding patterns

--- modules/RegexChatbot.py
+++ modules/RegexChatbot.py
@@ -9,14 +9,17 @@ class ChatBot:
         if chatbotResponse is None:
             chatbotResponse = {
                 r'hi|hello|hey|hai|hallo|halo': 'Halo, ada yang saya bisa bantu?',
-                r'siapa namamu?': 'Saya adalah ruled based chatbot yang dibuat oleh Alex & Ryan, mahasiswa DTETI FT UGM',
-                r'apa tujuanmu dibuat?': 'Memberikan informasi mengenai DTETI, mulai dari perkuliahan sampai kehidupan mahasiswa/i-nya',
-                r'saya|aku memiliki pertanyaan': 'Baiklah, apa pertanyaan Anda?',
-                r'apakah kamu tahu tentang (.*)': 'Tentu apakah {0} menurutmu menarik? Jelaskan pada saya!',
-                r'menurut (saya|aku), (.*)':'Aku setuju dengan pendapatmu!',
+                r'(.*)?siapa namamu?': 'Saya adalah ruled based chatbot yang dibuat oleh Alex & Ryan, mahasiswa DTETI FT UGM',
+                r'(.*)?apa tujuanmu dibuat?': 'Memberikan informasi mengenai DTETI, mulai dari perkuliahan sampai kehidupan mahasiswa/i-nya',
+                r'(.*)?saya|aku memiliki pertanyaan': 'Baiklah, apa pertanyaan Anda?',
+                r'(.*)?apakah kamu tahu tentang (.*)': 'Tentu apakah {0} menurutmu menarik? Jelaskan pada saya!',
+                r'(.*)?bolos(.*)': 'Waduh bolos itu tidak baik, coba ceritakan kenapa kamu bolos?',
+                r'(.*)?menyenangkan (.*)': 'Wah sangat menarik, seberapa menyenangkan hal itu',
+                r'(.*)?nilai (.*)': 'anda harus belajar giat supaya mendapat niali yang baik, coba kamu ceritakan pengalamanmu?',
+                r'(.*)?menurut (saya|aku), (.*)':'Aku setuju dengan pendapatmu!',
                 r'(.*) berkuliah di DTETI':'Saya sangat penasaran bagaimana rasanya, jelaskan pada saya!',
                 r'(.*)?sulit|susah (.*)': 'Bisakah anda jelaskan dimana susahnya?',
-                r'menurut (saya|aku), (susah|sulit)nya itu (.*)': 'Tenang saja, yang kamu rasakan itu valid. Tetap semangat ya!',
+                r'(.*)?menurut (saya|aku), (susah|sulit)nya itu (.*)': 'Tenang saja, yang kamu rasakan itu valid. Tetap semangat ya!',
                 r'(.*)?siapa yang paling (ganteng|baik|tidak rasis|cerdas|positif) di DTETI?': 'Wah jelas Alex Cinatra Hutasoit', #Easter Egg
                 r'thank you|thanks|terimakasih|makasih (.*)?': 'Sama-sama, jika Anda punya pertanyaan lebih lanjut silakan tanyakan saja!',
                 r'bye|goodbye|selamat tinggal|oke.*|baiklah|terimakasih': 'Goodbye! Have a great day!'
